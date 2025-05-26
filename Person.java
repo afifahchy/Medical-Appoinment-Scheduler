6bded11764febe955cf50d5825161a0564dee6de
Afifah Mansur chowdhury <afifahchowdhuryrodshi@gmail.com>
2025-05-26 14:25:56 +0600

    Person.java

---
+++ Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    String name;
+    int age;
+    String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
